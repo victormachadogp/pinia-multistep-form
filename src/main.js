ef6264927a9fe8a8d57b5c978c8874d6b15fb0e5
Victor Machado <victormachadogp@gmail.com>
2024-02-19 20:07:09 -0300

    Persist Pinia state

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp, watch } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
+
+if(localStorage.getItem("state")) {
+    pinia.state.value = JSON.parse(localStorage.getItem("state"))
+  }
+
+  watch(pinia.state, (state) => {
+    localStorage.setItem("state", JSON.stringify(state))
+  },
+  {deep: true}
+)
