5bfddab038a75126e630e0ed4232fde918d3baf4
Victor Machado <victormachadogp@gmail.com>
2024-02-23 19:39:45 -0300

    Home - Inscreasing size and reordering button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,79 +3,81 @@
       <!-- <img class="form-image" src="../assets/almond_blossom.jpeg" alt=""> -->
 
       <div class="form-image-wrapper"></div>
-      <section class="form">
-         <div class="mx-auto max-w-lg pt-10">
-            <h1 class="text-2xl mb-2">Personal Info</h1>
+      <section class="form flex flex-col justify-between">
+         <div class="">
+            <div class="mx-auto max-w-lg pt-10">
+               <h1 class="text-2xl mb-2">Personal Info</h1>
 
-            <p class="text-sm text-gray-400">Please provide your name, email addres, and phone number.</p>
+               <p class="text-sm text-gray-400">Please provide your name, email addres, and phone number.</p>
+            </div>
+
+            <form action="#" method="POST" class="px-6 lg:px-8 pt-10">
+               <div class="mx-auto max-w-lg">
+                  <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                     <div>
+                        <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900">First name</label>
+                        <div class="mt-2.5">
+                           <input
+                              v-model="name"
+                              type="text"
+                              name="first-name"
+                              id="first-name"
+                              autocomplete="given-name"
+                              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                           />
+                        </div>
+                     </div>
+                     <div>
+                        <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900">Last name</label>
+                        <div class="mt-2.5">
+                           <input
+                              v-model="lastName"
+                              type="text"
+                              name="last-name"
+                              id="last-name"
+                              autocomplete="family-name"
+                              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                           />
+                        </div>
+                     </div>
+                     <div class="sm:col-span-1">
+                        <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
+                        <div class="mt-2.5">
+                           <input
+                              v-model="email"
+                              type="email"
+                              name="email"
+                              id="email"
+                              autocomplete="email"
+                              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                           />
+                        </div>
+                     </div>
+                     <div class="sm:col-span-1">
+                        <label for="phone-number" class="block text-sm font-semibold leading-6 text-gray-900">Phone number</label>
+                        <div class="mt-2.5">
+                           <input
+                              v-model="phone"
+                              type="tel"
+                              name="phone-number"
+                              id="phone-number"
+                              autocomplete="tel"
+                              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                           />
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </form>
          </div>
 
-         <form action="#" method="POST" class="px-6 lg:px-8 pt-10">
-            <div class="mx-auto max-w-lg">
-               <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                  <div>
-                     <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900">First name</label>
-                     <div class="mt-2.5">
-                        <input
-                           v-model="name"
-                           type="text"
-                           name="first-name"
-                           id="first-name"
-                           autocomplete="given-name"
-                           class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                     </div>
-                  </div>
-                  <div>
-                     <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900">Last name</label>
-                     <div class="mt-2.5">
-                        <input
-                           v-model="lastName"
-                           type="text"
-                           name="last-name"
-                           id="last-name"
-                           autocomplete="family-name"
-                           class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                     </div>
-                  </div>
-                  <div class="sm:col-span-1">
-                     <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
-                     <div class="mt-2.5">
-                        <input
-                           v-model="email"
-                           type="email"
-                           name="email"
-                           id="email"
-                           autocomplete="email"
-                           class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                     </div>
-                  </div>
-                  <div class="sm:col-span-1">
-                     <label for="phone-number" class="block text-sm font-semibold leading-6 text-gray-900">Phone number</label>
-                     <div class="mt-2.5">
-                        <input
-                           v-model="phone"
-                           type="tel"
-                           name="phone-number"
-                           id="phone-number"
-                           autocomplete="tel"
-                           class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                     </div>
-                  </div>
-               </div>
-
-               <div class="border-t border-gray-300 mt-24"></div>
-
-               <div class="mt-8 flex justify-end">
-                  <router-link to="/about">
-                     <DynamicSubmitButton />
-                  </router-link>
-               </div>
+         <div class="mx-auto max-w-lg w-full pb-10">
+            <div class="flex justify-end border-t border-gray-300">
+               <router-link class="mt-10" to="/about">
+                  <DynamicSubmitButton />
+               </router-link>
             </div>
-         </form>
+         </div>
       </section>
    </main>
 </template>
@@ -104,7 +106,7 @@ const handleSubmit = () => {
    justify-content: center;
    background-color: white;
    /* max-width: 1100px; */
-   height: 500px;
+   height: 650px;
    border-radius: 10px;
 }
 
