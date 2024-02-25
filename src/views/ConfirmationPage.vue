7e95a1b2db013251eda8c2ff59d53d671fe3e8bb
Victor Machado <victormachadogp@gmail.com>
2024-02-24 22:45:05 -0300

    Confirmation page - organizing elements

--- src/views/ConfirmationPage.vue
+++ src/views/ConfirmationPage.vue
@@ -2,80 +2,85 @@
    <main class="main">
       <!-- <img class="form-image" src="../assets/almond_blossom.jpeg" alt=""> -->
       <div class="form-image-wrapper"></div>
-      <section class="form">
+      <section class="form flex flex-col justify-between">
          <div class="mx-auto max-w-lg pt-10">
-            <h1 class="text-2xl mb-2">Please Review Your Info</h1>
+            <div>
+               <h1 class="text-2xl mb-2">Please Review Your Info</h1>
 
-            <p class="text-sm text-gray-400">Please provide your name, email addres, and phone number.</p>
-         </div>
-
-         <div class="mx-auto max-w-lg pt-10">
-            <div class="mt-6 border-t border-gray-100">
-               <dl class="divide-y divide-gray-100">
-                  <div class="px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                     <dt class="text-sm font-medium leading-6 text-gray-900">Full name</dt>
-                     <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                        <span class="flex-grow">{{ fullName }}</span>
-                        <span class="ml-4 flex-shrink-0">
-                           <router-link to="/">
-                              <button @click="updateInfo" type="button" class="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">
-                                 Update
-                              </button>
-                           </router-link>
-                        </span>
-                     </dd>
-                  </div>
-                  <div class="px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                     <dt class="text-sm font-medium leading-6 text-gray-900">Email</dt>
-                     <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                        <span class="flex-grow">{{ email }}</span>
-                        <span class="ml-4 flex-shrink-0">
-                           <button type="button" class="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">Update</button>
-                        </span>
-                     </dd>
-                  </div>
-                  <div class="px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                     <dt class="text-sm font-medium leading-6 text-gray-900">Phone</dt>
-                     <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                        <span class="flex-grow">{{ phone }}</span>
-                        <span class="ml-4 flex-shrink-0">
-                           <button type="button" class="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">Update</button>
-                        </span>
-                     </dd>
-                  </div>
-                  <div class="px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                     <dt class="text-sm font-medium leading-6 text-gray-900">Message</dt>
-                     <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                        <span class="flex-grow">{{ truncatedMessage }}</span>
-                        <span class="ml-4 flex-shrink-0">
-                           <button type="button" class="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">Update</button>
-                        </span>
-                     </dd>
-                  </div>
-
-                  <div class="px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                     <dt class="text-sm font-medium leading-6 text-gray-900">Picked Option</dt>
-                     <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                        <span class="flex-grow">{{ pickedOption }}</span>
-                        <span class="ml-4 flex-shrink-0">
-                           <button type="button" class="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">Update</button>
-                        </span>
-                     </dd>
-                  </div>
-
-                  <div class="px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                     <dt class="text-sm font-medium leading-6 text-gray-900">Subscription Option</dt>
-                     <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                        <span class="flex-grow">{{ subscriptionOption }}</span>
-                        <span class="ml-4 flex-shrink-0">
-                           <button type="button" class="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">Update</button>
-                        </span>
-                     </dd>
-                  </div>
-               </dl>
+               <p class="text-sm text-gray-400">Please provide your name, email addres, and phone number.</p>
             </div>
 
-            <div class="border-t border-gray-300 mt-24"></div>
+            <div class="mx-auto max-w-lg pt-10">
+               <div class="mt-6 border-t border-gray-100">
+                  <dl class="divide-y divide-gray-100">
+                     <div class="px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Full name</dt>
+                        <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                           <span class="flex-grow">{{ fullName }}</span>
+                           <span class="ml-4 flex-shrink-0">
+                              <router-link to="/">
+                                 <button
+                                    @click="updateInfo"
+                                    type="button"
+                                    class="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500"
+                                 >
+                                    Update
+                                 </button>
+                              </router-link>
+                           </span>
+                        </dd>
+                     </div>
+                     <div class="px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Email</dt>
+                        <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                           <span class="flex-grow">{{ email }}</span>
+                           <span class="ml-4 flex-shrink-0">
+                              <button type="button" class="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">Update</button>
+                           </span>
+                        </dd>
+                     </div>
+                     <div class="px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Phone</dt>
+                        <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                           <span class="flex-grow">{{ phone }}</span>
+                           <span class="ml-4 flex-shrink-0">
+                              <button type="button" class="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">Update</button>
+                           </span>
+                        </dd>
+                     </div>
+                     <div class="px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Message</dt>
+                        <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                           <span class="flex-grow">{{ truncatedMessage }}</span>
+                           <span class="ml-4 flex-shrink-0">
+                              <button type="button" class="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">Update</button>
+                           </span>
+                        </dd>
+                     </div>
+
+                     <div class="px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Picked Option</dt>
+                        <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                           <span class="flex-grow">{{ pickedOption }}</span>
+                           <span class="ml-4 flex-shrink-0">
+                              <button type="button" class="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">Update</button>
+                           </span>
+                        </dd>
+                     </div>
+
+                     <div class="px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                        <dt class="text-sm font-medium leading-6 text-gray-900">Subscription Option</dt>
+                        <dd class="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                           <span class="flex-grow">{{ subscriptionOption }}</span>
+                           <span class="ml-4 flex-shrink-0">
+                              <button type="button" class="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">Update</button>
+                           </span>
+                        </dd>
+                     </div>
+                  </dl>
+               </div>
+
+               <!-- <div class="border-t border-gray-300 mt-24"></div>
 
             <div class="mt-8 flex justify-end items-center">
                <router-link to="/subscription">
@@ -88,6 +93,26 @@
                >
                   Next Step
                </button>
+            </div> -->
+            </div>
+         </div>
+
+         <div class="mx-auto max-w-lg w-full pb-10">
+            <div class="flex justify-end border-t border-gray-300">
+               <div class="flex items-center mt-10">
+                  <router-link to="/subscription">
+                     <button class="text-gray-400 mx-5 font-medium">Go Back</button>
+                  </router-link>
+                  <router-link to="#">
+                     <button
+                        @click="handleSubmitFunction"
+                        type="button"
+                        class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                     >
+                        Finish
+                     </button>
+                  </router-link>
+               </div>
             </div>
          </div>
       </section>
