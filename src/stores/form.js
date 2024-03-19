abf8c6bf597497b8db65a893198c347df9dda7a8
Victor Machado <victormachadogp@gmail.com>
2024-03-19 00:32:01 -0300

    Fixing store to only one ref() per object

--- src/stores/form.js
+++ src/stores/form.js
@@ -1,34 +1,34 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useFormStore = defineStore('form', () => {
+export const useFormStore = defineStore('userForm', () => {
 
-   const user = {
-      name: ref(''),
-      lastName: ref(''),
-      email: ref(''),
-      phone: ref(''),
+   const user = ref({
+      name: '',
+      lastName: '',
+      email: '',
+      phone: '',
       data: {
-         message: ref(''),
-         pickedOption: ref(''),
-         subscriptionOption: ref(''),
+         message: '',
+         pickedOption: '',
+         subscriptionOption: '',
       }
-   }
+   })
 
    const updatingInfo = ref('')
 
    const fullName = computed(() => {
-      return `${user.name.value} ${user.lastName.value}`
+      return `${user.value.name} ${user.value.lastName}`
    });
 
    const $reset = function () {
-      user.name = '';
-      user.lastName = '';
-      user.email = '';
-      user.phone = '';
-      user.data.message = '';
-      user.data.pickedOption = '';
-      user.data.subscriptionOption = '';
+      user.value.name = '';
+      user.value.lastName = '';
+      user.value.email = '';
+      user.value.phone = '';
+      user.value.data.message = '';
+      user.value.data.pickedOption = '';
+      user.value.data.subscriptionOption = '';
    }
 
    return { user, updatingInfo, fullName, $reset }
